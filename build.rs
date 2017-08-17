21959cfda4d7cff4b6d04fdee48ad2afdaf17972
xianyou.cyq <xianyou.cyq@alibaba-inc.com>
2017-08-17 14:59:23 +0800

    add manul ffi

--- build.rs
+++ build.rs
@@ -9,17 +9,4 @@ fn main() {
     let dst = Config::new("libwebp").build_target("webp").build();
     println!("cargo:rustc-link-search=native={}/build", dst.display());
     println!("cargo:rustc-link-lib=static=webp");
-
-    let bindings = Builder::default()
-        .header("wrapper.h")
-        .trust_clang_mangling(false)
-        .generate()
-        .expect("Unable to generate bindings");
-
-    println!("bindgs-generate-bindings");
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    println!("out_dir: {:?}", out_path);
-    bindings
-        .write_to_file(out_path.join("ffi.rs"))
-        .expect("Couldn't write bindings!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-include!(concat!(env!("OUT_DIR"), "/ffi.rs"));
+include!("./ffi.rs");
 
 #[cfg(test)]
 mod tests {
